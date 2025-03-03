07e883068f93f38a71672b21484c8275d98020d3
Milad Bassil <62559207+sewerrenegade@users.noreply.github.com>
2025-03-03 16:51:18 +0100

    fixed import problem

--- __init__.py
+++ __init__.py
@@ -3,7 +3,8 @@ import os
 
 # Get the directory of this file (which is mypackage/)
 package_dir = os.path.dirname(os.path.abspath(__file__))
-
+parent_dir = os.path.dirname(package_dir)
 # Add this directory to sys.path (if not already added)
-if package_dir not in sys.path:
-    sys.path.append(package_dir)
+if parent_dir not in sys.path:
+    sys.path.append(parent_dir)
+    print(f"added this path:{parent_dir}")
